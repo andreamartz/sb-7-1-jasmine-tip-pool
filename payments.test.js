6f2cf2d4750eba67befe32de5a0c2f89a29d82aa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-13 16:49:41 -0500

    Add setup & teardown logic and test createCurPayment

--- payments.test.js
+++ payments.test.js
@@ -1,9 +1,23 @@
 describe("Payments tests (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
+    allPayments = {};
+    paymentId = 0;
   });
 
-  it("should ... on createCurPayment()", function () {});
+  it("should ... on submitPaymentInfo()", function () {});
+
+  it("should create an object containing correct bill amt, tip amt, and tip percent on createCurPayment()", function () {
+    billAmtInput.value = 80;
+    tipAmtInput.value = 16;
+    expect(createCurPayment().billAmt).toEqual("80");
+    expect(createCurPayment().tipAmt).toEqual("16");
+    expect(createCurPayment().tipPercent).toEqual(20);
+  });
+
+  it("should not create an object on createCurPayment() when no bill amount", function () {
+    expect(createCurPayment()).toEqual(undefined);
+  });
 
   it("should ... on appendPaymentTable()", function () {});
 
@@ -11,5 +25,7 @@ describe("Payments tests (with setup and tear-down)", function () {
 
   afterEach(function () {
     // teardown logic
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
   });
 });
