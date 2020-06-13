9eb351d69af59e8fce337ba13d7dd09dafd05489
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-13 16:30:57 -0500

    Add skeleton structure for payments test file

---
+++ payments.test.js
@@ -0,0 +1,15 @@
+describe("Payments tests (with setup and tear-down)", function () {
+  beforeEach(function () {
+    // initialization logic
+  });
+
+  it("should ... on createCurPayment()", function () {});
+
+  it("should ... on appendPaymentTable()", function () {});
+
+  it("should ... on updateSummary()", function () {});
+
+  afterEach(function () {
+    // teardown logic
+  });
+});
